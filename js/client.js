ccac1bf2ba211bed4b9d580e168da4a0451a8c1f
Mukesh Kr. Gupta <rg37090@gmail.com>
2023-12-20 21:56:41 +0530

    first commit

--- js/client.js
+++ js/client.js
@@ -1,3 +1,4 @@
+
 const socket = io('http://localhost:8000')
 
 
@@ -14,13 +15,6 @@ const append = (message, position)=>{
     messsageContainer.append(messageElement);
 }
 
-form.addEventListener('submit',(e)=>{
-  e.preventDefault();
-  const message = messageInput.value; 
-  append(`you: ${message}`,'right') 
-  socket.emit('send', message)
-  messageInput.value=''
-})
 
 const uname = prompt("Enter your name to join");
 socket.emit('new-user-joined',uname);
@@ -32,3 +26,11 @@ socket.on('user-joined',name=>{
 socket.on('recieve',data=>{
     append(`${data.name}: ${data.message}`,'left')
 });
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  const message = messageInput.value; 
+  append(`you: ${message}`,'right') 
+  socket.emit('send', message)
+  messageInput.value=''
+})
--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
+const express = require('express');
+const http = require('http');
 const io = require('socket.io')(8000)
 const cors = require('cors')
 
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
 const user = {};
 
-io.on('connection',socket=>{
+PORT = 5500;
+
+io.on('connection', socket => {
 
     socket.on('new-user-joined',user_name=>{
         console.log(user_name + " connected");
@@ -15,3 +24,8 @@ io.on('connection',socket=>{
         socket.broadcast.emit('receive', { message: message, user_name: user[socket.id]});
     });
 });
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
